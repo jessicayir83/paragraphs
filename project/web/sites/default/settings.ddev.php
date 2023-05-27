dacb67d0b6d19b10f9593d4fff338b2b3415ea97
Angel RQ <angel.rodriguez@ucr.ac.cr>
2023-05-27 21:41:28 +0000

    push de prueba

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'KiAzYVGsukOvjgzdnpZRWngqirQOTTLlobsdsQsItUOdwftgFWAYrcirnUvdfQNt';
+$settings['hash_salt'] = 'pGxBZosMgOmmpJGvNyICeLfJApHEdqUTQbJkYZessFTgaTVrknDgBfdFwRxllEFh';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
