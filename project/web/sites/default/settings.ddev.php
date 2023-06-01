ea7ca1861e66a2f48e270f11354c543a4f8c70f0
Andrey Fernández <melcay.14@gmail.com>
2023-06-01 08:23:25 +0000

    user profile

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'IUFtdmClSDCegygyiSGbKCtuVgdfDYOvHKxFjLMCtonhRPFWymsIWrXctRNQhwjm';
+$settings['hash_salt'] = 'OOmnxFfaOLgGJyVkcazhxLEnTGQPnaNwdBmNuGcYjDZgpYESxSSzGiAgCEoodgQc';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
