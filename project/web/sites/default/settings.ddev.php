494b0627a4da502fdc5f8b1c7c123944397afd84
Andrey Fernández <melcay.14@gmail.com>
2023-06-05 22:00:06 +0000

    fix loading

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'NgzvAYBoGFOkvbzBQgvjJtrQYPMmkknHQnBGZllwqRanqdYTIWnDTEdXyunuUmOx';
+$settings['hash_salt'] = 'MxMkirVQVpziLFxEBtgHsfpEiHxvSHYbBlHGqJQFJxhkOEhgKxwTIwihPCvZYKFI';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
