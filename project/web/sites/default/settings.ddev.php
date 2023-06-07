4b16927a1b380f7aaba3f143036ca02de15b35b7
Jessica Gonzalez <yirgonzales7161@gmail.com>
2023-06-07 20:09:53 +0000

    commit

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'MxMkirVQVpziLFxEBtgHsfpEiHxvSHYbBlHGqJQFJxhkOEhgKxwTIwihPCvZYKFI';
+$settings['hash_salt'] = 'SEKvlotxAqgWuXyAMnTccmHMDpfyPsXGGEOUCrgcvZrENwDRuusroZSQJKBREZwl';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
